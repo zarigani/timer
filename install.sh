5b52b508fcbb7f4060ddfd8f38bec6a35663881c
zariganitosh <XXXX@example.com>
2013-02-22 14:14:45 +0900

    Modify install.sh "mv" to "cp -p".

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@ INSTALL_DIRECTORY=${1:-/usr/local/bin}
 [ $# = 0 ] && mkdir -p /usr/local/bin
 
 cd `dirname $0`
-mv timer $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/timer
-mv timer-sync-wake $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/timer-sync-wake
-sudo mv timer-add-schedule $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/timer-add-schedule
-sudo mv timer-rm-schedule $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/timer-rm-schedule
+cp -p timer $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/timer
+cp -p timer-sync-wake $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/timer-sync-wake
+sudo cp -p timer-add-schedule $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/timer-add-schedule
+sudo cp -p timer-rm-schedule $INSTALL_DIRECTORY && echo install $INSTALL_DIRECTORY/timer-rm-schedule
 
 cd $INSTALL_DIRECTORY
 chmod +x timer timer-sync-wake timer-add-schedule timer-rm-schedule && echo chmod +x timer timer-sync-wake timer-add-schedule timer-rm-schedule
